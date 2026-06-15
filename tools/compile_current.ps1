eed57c55de1b05ebe6a87774fce3e6ee741d43ca
soich <soichi@soichi>
2026-06-16 04:19:12 +0900

    Add VS Code compile task for MT4

---
+++ tools/compile_current.ps1
@@ -0,0 +1,8 @@
+﻿param(
+    [string]$SourceFile,
+    [string]$LogFile
+)
+
+$MetaEditor = "C:\Program Files (x86)\Rakuten MetaTrader 4\MetaEditor.exe"
+
+& $MetaEditor "/compile:`"$SourceFile`"" "/log:`"$LogFile`""
